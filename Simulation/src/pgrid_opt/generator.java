e119a3e41300f5c42608cb2d03c76fa8514b3d9d
Julien van der Land <J.van.der.Land.2@student.rug.nl>
2016-04-09 11:42:27 +0200

    new gitignore, some formatting changes.

--- Simulation/src/pgrid_opt/generator.java
+++ Simulation/src/pgrid_opt/generator.java
@@ -1,45 +1,53 @@
- package pgrid_opt;
+package pgrid_opt;
 
-public class generator
-   extends node
- {
-   private float maxp;
-  
-  private float minp;
-   
- private float coef;
-  private String type;
-  public generator(float min, float max, float coef, String type)
-   {
-    this.maxp = max;
-this.minp = min;
-this.coef = coef;
-setType(type);
-}
+public class generator extends node {
+	private float maxp;
 
-public float getMaxP() { return this.maxp; }
+	private float minp;
 
-public void setMaxP(float maxp) {
-this.maxp = maxp;
-}
+	private float coef;
+	private String type;
 
-public float getMinP() { return this.minp; }
+	public generator(float min, float max, float coef, String type) {
+		this.maxp = max;
+		this.minp = min;
+		this.coef = coef;
+		setType(type);
+	}
 
-public void setMinP(float minp) {
-this.minp = minp;
-}
+	public float getMaxP() {
+		return this.maxp;
+	}
 
-public boolean isRenew() { return false; }
+	public void setMaxP(float maxp) {
+		this.maxp = maxp;
+	}
 
-public float getCoef() {
-return this.coef;
-}
+	public float getMinP() {
+		return this.minp;
+	}
 
-public void setCoef(float coef) { this.coef = coef; }
+	public void setMinP(float minp) {
+		this.minp = minp;
+	}
 
-public String getType() {
-return this.type;
-}
+	public boolean isRenew() {
+		return false;
+	}
 
-public void setType(String type) { this.type = type; }
+	public float getCoef() {
+		return this.coef;
+	}
+
+	public void setCoef(float coef) {
+		this.coef = coef;
+	}
+
+	public String getType() {
+		return this.type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
 }
