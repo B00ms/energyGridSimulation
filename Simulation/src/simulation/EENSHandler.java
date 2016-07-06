677723fdf9d3e587ee080a6f2afc797df88c0e98
ejay <jager.erik@gmail.com>
2016-07-06 14:14:06 +0200

    Moved EENS stuff to separate class

---
+++ Simulation/src/simulation/EENSHandler.java
@@ -0,0 +1,69 @@
+package simulation;
+
+import graph.Graph;
+import java.util.List;
+
+/**
+ * Created by ejay on 06/07/16.
+ */
+public class EENSHandler {
+
+    private ProductionLoadHandler productionLoadHandler = new ProductionLoadHandler();
+
+    public Double calculateHourlyEENS(Graph realSimulationGraph){
+        double hourlyEENS = 0;
+
+        double realLoad 		= productionLoadHandler.calculateLoad(realSimulationGraph);
+        double satisfiedLoad 	= productionLoadHandler.calculateSatisfiedLoad(realSimulationGraph);
+
+        // if there is expected energy not supplied then count it as shedded load
+        System.out.println("EENS: " + (realLoad-satisfiedLoad));
+
+        if((realLoad - satisfiedLoad) > 0)
+            hourlyEENS += (realLoad - satisfiedLoad);
+
+        if(hourlyEENS < 0)
+            hourlyEENS = 0;
+
+        return hourlyEENS;
+    }
+
+    public static Double calculateRealEENS(List<Double> listEENS){
+        double sumEENS = 0;
+        double realEENS = 0;
+
+        if(listEENS.size() > 0) {
+
+            for (int i = 0; i < listEENS.size(); i++) {
+                sumEENS += listEENS.get(i);
+            }
+
+            realEENS = sumEENS / listEENS.size();
+        }
+
+        return realEENS;
+    }
+
+    public boolean checkEENSConvergence(List<Double> listEENS, double EENSConvergenceThreshold){
+        double sumEENS = 0;
+        double avgEENS = 0;
+        boolean EENSConvergence = false;
+
+        if(listEENS.size() > 1){
+            for (int i =0; i < listEENS.size()-1; i++){
+                sumEENS += listEENS.get(i);
+            }
+
+            avgEENS = sumEENS / listEENS.size()-1;
+
+            double convergence = Math.abs(avgEENS - listEENS.get(listEENS.size()-1));
+            System.out.println("Convergence: "+ convergence +", Threshold: "+ EENSConvergenceThreshold);
+            // stop the simulation when converged
+            if(convergence <= EENSConvergenceThreshold){
+                EENSConvergence = true;
+            }
+        }else{}
+
+        return EENSConvergence;
+    }
+}
